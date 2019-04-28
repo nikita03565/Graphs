74eebc0384e1a69682aed4d5091ac5726d9c7155
Nikita Povalyaev <nikita03565@gmail.com>
2019-04-28 23:50:28 +0300

    Little rename

--- distance.py
+++ distance.py
@@ -1,8 +1,8 @@
-def single_source_shortest_path_length(G, source, cutoff=None):
+def single_source_shortest_path_length(g, source, cutoff=None):
     if cutoff is None:
         cutoff = float('inf')
     nextlevel = {source: 1}
-    return dict(single_shortest_path_length(G.adj, nextlevel, cutoff))
+    return dict(single_shortest_path_length(g.adj, nextlevel, cutoff))
 
 
 def single_shortest_path_length(adj, firstlevel, cutoff):
@@ -22,51 +22,51 @@ def single_shortest_path_length(adj, firstlevel, cutoff):
     del seen
 
 
-def eccentricity(G, v=None, sp=None):
+def eccentricity(g, v=None, sp=None):
     e = {}
-    for n in G.nbunch_iter(v):
+    for n in g.nbunch_iter(v):
         if sp is None:
-            length = single_source_shortest_path_length(G, n)
+            length = single_source_shortest_path_length(g, n)
         else:
             length = sp[n]
 
         e[n] = max(length.values())
 
-    if v in G:
+    if v in g:
         return e[v]
     else:
         return e
 
 
-def diameter(G, e=None):
+def diameter(g, e=None):
     if e is None:
-        e = eccentricity(G)
+        e = eccentricity(g)
     return max(e.values())
 
 
-def periphery(G, e=None):
+def periphery(g, e=None):
     if e is None:
-        e = eccentricity(G)
+        e = eccentricity(g)
     diameter = max(e.values())
     p = [v for v in e if e[v] == diameter]
     return p
 
 
-def radius(G, e=None):
+def radius(g, e=None):
     if e is None:
-        e = eccentricity(G)
+        e = eccentricity(g)
     return min(e.values())
 
 
-def center(G, e=None):
+def center(g, e=None):
     if e is None:
-        e = eccentricity(G)
+        e = eccentricity(g)
     radius = min(e.values())
     p = [v for v in e if e[v] == radius]
     return p
 
 
-def average_shortest_path_length(G):
-    n = G.order()
-    s = sum(l for u in G for l in single_source_shortest_path_length(G, u).values())
+def average_shortest_path_length(g):
+    n = g.order()
+    s = sum(l for u in g for l in single_source_shortest_path_length(g, u).values())
     return s / (n * (n - 1))
